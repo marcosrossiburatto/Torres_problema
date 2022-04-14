b88e492c08c909c50a81010fb3d01fb32f9bc3eb
Marcos Buratto <resoluk@gmail.com>
2022-04-14 20:20:15 -0300

    atualizacao

--- TorresHanoiDemo-estudo02.js
+++ TorresHanoiDemo-estudo02.js
@@ -1,19 +1,18 @@
 const mbObj = window.matchMedia("(max-width: 700px)");
-mbObj.addEventListener("load", marcos, false);
+mbObj.addEventListener("change", marcos, false);
+let box02 = document.createElement("div");
 
 function marcos(x) {
       if (x.matches) {
-            this.box02 = document.createElement ("div");
-            this.box02.style.color = "#000000";
+           box02.style.color = "#000000";
       } else {
-            this.box02 = document.createElement ("div");
-            this.box02.style.color = "#ff0000";
+            box02.style.color = "#ff0000";
       };
 }
 
 function titleTextHanoi() {
 marcos(mbObj);
-this.box02.textContent = "As Torres de Hanói é um quebra-cabeça que consiste em uma base contendo três pinos, em um dos quais são dispostos alguns discos uns sobre os outros, em ordem crescente de diâmetro, de cima para baixo.";
+box02.textContent = "As Torres de Hanói é um quebra-cabeça que consiste em uma base contendo três pinos, em um dos quais são dispostos alguns discos uns sobre os outros, em ordem crescente de diâmetro, de cima para baixo.";
  screen.appendChild(box02);
 }
 
